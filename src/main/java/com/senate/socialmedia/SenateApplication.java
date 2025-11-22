9a60f4aaef08db963620f7aff826832ecdbfc1e9
ufukeraymortas <ufukeraymortas@gmail.com>
2025-11-22 21:31:16 +0300

    Final Driver Class Manual Load

--- src/main/java/com/senate/socialmedia/SenateApplication.java
+++ src/main/java/com/senate/socialmedia/SenateApplication.java
@@ -2,12 +2,23 @@ package com.senate.socialmedia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.annotation.PostConstruct; // Yeni eklenecek
 
 @SpringBootApplication
 public class SenateApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SenateApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SenateApplication.class, args);
+    }
+    
+    // YENİ EKLENEN METOT: Uygulama başlamadan önce PostgreSQL driver'ı tanıtır.
+    @PostConstruct
+    public void setPostgresqlDriver() {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            // Bu normalde asla olmaz, çünkü kütüphanemiz (pom.xml) doğru.
+            e.printStackTrace(); 
+        }
+    }
 }
