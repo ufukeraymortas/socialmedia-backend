ad19207188468cfbbbf52e1aee3f54ca0858e56b
ufukeraymortas <ufukeraymortas@gmail.com>
2025-11-22 21:42:30 +0300

    Minimal changes in main class

--- src/main/java/com/senate/socialmedia/SenateApplication.java
+++ src/main/java/com/senate/socialmedia/SenateApplication.java
@@ -11,13 +11,11 @@ public class SenateApplication {
         SpringApplication.run(SenateApplication.class, args);
     }
     
-    // YENİ EKLENEN METOT: Uygulama başlamadan önce PostgreSQL driver'ı tanıtır.
     @PostConstruct
     public void setPostgresqlDriver() {
         try {
             Class.forName("org.postgresql.Driver");
         } catch (ClassNotFoundException e) {
-            // Bu normalde asla olmaz, çünkü kütüphanemiz (pom.xml) doğru.
             e.printStackTrace(); 
         }
     }
