8ab2695061e60533c22585aa7b1b78954a628f8f
ufukeraymortas <ufukeraymortas@gmail.com>
2025-12-27 20:05:36 +0300

    U3

--- src/main/java/com/senate/socialmedia/config/WebConfig.java
+++ src/main/java/com/senate/socialmedia/config/WebConfig.java
@@ -3,23 +3,16 @@ package com.senate.socialmedia.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-	    registry.addMapping("/**") // Tüm yollara izin ver
-	            .allowedOrigins("*") // HANGİ ADRESLERDEN İSTEK GELDİĞİ ÖNEMLİ DEĞİL (Vercel, Render, Localhost)
-	            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-	            .allowedHeaders("*");
-	}
-	@Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // "/uploads/resim.jpg" isteği gelirse...
-        registry.addResourceHandler("/uploads/**")
-                // ...git "uploads" klasörüne bak. ("file:" demek diskten oku demek)
-                .addResourceLocations("file:./uploads/");
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // Tüm adreslere (/**) gelen isteklere izin ver
+        registry.addMapping("/**")
+                .allowedOrigins("*") // Vercel, Localhost vs. her yerden erişime izin ver
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Tüm işlemleri aç
+                .allowedHeaders("*"); // Tüm başlıkları kabul et
     }
 }
